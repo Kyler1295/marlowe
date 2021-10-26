df12eec81c2fcb434e7674dd2e1e698bc2a11c6e
Kyle Rosen <kyler1295@gmail.com>
2021-10-26 10:32:16 -0400

    Events CHange
    Changed Events to it's own file for global use.

--- vineyard/src/views/Events.vue
+++ vineyard/src/views/Events.vue
@@ -27,6 +27,7 @@
 
 <script>
 import Hero from '@/components/Hero';
+import {marloweEvents} from '@/data/events.js';
 
 export default {
   name: "Events",
@@ -35,21 +36,7 @@ export default {
   },
   data() {
     return {
-      events:
-      [
-        {
-          "name": "Hallowine",
-          "date": "October 26th, 2021",
-          "photo": "Hallowine_28.png",
-          "description": "Think you're the best dresser in the city? Take a drive up to Marlowe Vineyards on Tuesday October 26th. Come dressed in your best halloween costume and enter the contest for a chance to win $5k. Our esteemed panel of judges will vote on the best costume of the night. After the competition our 6 judges will be offering themselves to the highest bidder for a romantic date night for 2. Ever wanted to talk to our owner Mr. Endeavour Jackson about a law you want passed? Now's your chance!"
-        },
-        {
-          "name": "Awakening",
-          "date": "October 31st, 2021",
-          "photo": "Awakening.png",
-          "description": "Description Needed"
-        }
-      ]
+      events: marloweEvents
     }
   }
 }
